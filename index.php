b7474477a1c95b1a9426de052fa072f2c496372b
AnthoFoxo <anthofoxo@gmail.com>
2023-11-25 14:27:26 -0500

    webkit scrollbar styles, landing page cleanup

--- index.php
+++ index.php
@@ -29,17 +29,20 @@
             ?>
         </div>
         <div>
-            ThumpNet is a custom level host for <a href="https://thumpergame.com/">Thumper</a>.
-            This is a succesor and more permanent solution to the efforts by Bigphish.
-            I'm <a href="discord://-/users/218415631479996417">@anthofoxo</a>, lead dev of the website and api.
-            Join the <a href="https://discord.gg/FU2X9z4ttJ">thumper discord</a>.
+            <div style="margin-bottom:8px;">
+                ThumpNet is a custom level host for <a href="https://thumpergame.com/">Thumper</a>.
+                This is a succesor and more permanent solution to the efforts by Bigphish.
+                I'm <a href="discord://-/users/218415631479996417">@anthofoxo</a>, lead dev of the website and api.
+                Join the <a href="https://discord.gg/FU2X9z4ttJ">thumper discord</a>.
 
-            <a href="https://github.com/CocoaMix86/Thumper-Custom-Level-Editor/releases/tag/2.0">Thumper custom level editor 2.0</a>
-            <a href="https://docs.google.com/document/d/1zwrpMhfugF7f_sxgpWUM9_cnOXtubOyFIqd7TCRryxM">Thumper manual 2.0</a>
+                <a href="https://github.com/CocoaMix86/Thumper-Custom-Level-Editor/releases/tag/2.0">Thumper custom level editor 2.0</a>
+                <a href="https://docs.google.com/document/d/1zwrpMhfugF7f_sxgpWUM9_cnOXtubOyFIqd7TCRryxM">Thumper manual 2.0</a>
+            </div>
 
-            <?php include("difficulty_table.html");?>
+            <span style="display:none;">
+                <?php include("difficulty_table.html");?>
+            </span>
 
-            <h1>Levels</h1>
             <div id="levels"></div>
         </div>
     </body>
